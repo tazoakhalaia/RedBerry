a4b22fd7a7ca3fbbfed4174322239eddc8dcd641
unknown <darknemiroff@gmail.com>
2022-08-29 19:20:35 +0400

    post request

--- laptopinformation/laptopinfo.js
+++ laptopinformation/laptopinfo.js
@@ -43,9 +43,20 @@ const mexsierebistipi = document.querySelector('.rmtype');
 const tarigi = document.querySelector('.ndate');
 const fasi = document.querySelector('.nprice');
 const laptopneworold = document.querySelector('.mdgomareoba');
+const body = document.querySelector('body');
+const saveInfo = document.querySelector('.save');
+
+upload.addEventListener('change', ()=>{
+    if (upload.files && upload.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function () {
+            localStorage.setItem("foto",reader.result);
+        }
+        reader.readAsDataURL(upload.files[0]);
+    }
+})
 
 function save() {
-    localStorage.setItem("image", JSON.stringify(upload.src));
     localStorage.setItem("laptopname", JSON.stringify(laptopname.value));
     localStorage.setItem("brand", JSON.stringify(brendi.value));
     localStorage.setItem("cpu", JSON.stringify(cpuoption.value));
@@ -56,4 +67,27 @@ function save() {
     localStorage.setItem("date", JSON.stringify(tarigi.value));
     localStorage.setItem("price", JSON.stringify(fasi.value));
     localStorage.setItem("new or old", JSON.stringify(laptopneworold.value));
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: laptopname.value,
+            brendii: brendi.value,
+            surati: upload.value,
+            cpu: cpuoption.value,
+            birtvii: birtvi.value,
+            nakaddi: nakadi.value,
+            ramm: rami.value,
+            ramtype: mexsierebistipi.value,
+            dro: tarigi.value,
+            tanxa: fasi.value,
+            axalianmeoradi: laptopneworold.value
+        }),
+        headers: {
+            "Content-Type":"application/json; charset=UTF-8"
+        }
+    }).then(function(responese){
+        return responese.json();
+    }).then(function(datas){
+        console.log(datas);
+    })
 }
